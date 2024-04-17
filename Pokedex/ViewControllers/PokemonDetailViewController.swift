d05fb6e325b96ffe20022300fb6272c2c02b7878
Jacob Davis <jacobdofficial@gmail.com>
2024-04-17 16:32:39 -0600

    Updated Constraints

--- Pokedex/ViewControllers/PokemonDetailViewController.swift
+++ Pokedex/ViewControllers/PokemonDetailViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
     
+    // Outlets relating to Pokemon images
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonTypingLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var imageSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var generationIntroducedLabel: UILabel!
     
+    //Evolution chain data labels
+    @IBOutlet weak var previousEvolutionLabel: UILabel!
+    @IBOutlet weak var nextEvolutionLabel: UILabel!
+    
+    // Base stats labels
+    @IBOutlet weak var hpStatLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var specialAttackLabel: UILabel!
+    @IBOutlet weak var specialDefenseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    // Damage relations labels
+    @IBOutlet weak var typeStrengthsLabel: UILabel!
+    @IBOutlet weak var typeWeaknessLabel: UILabel!
+    
+    // Abilites tableview
+    @IBOutlet weak var abilitiesTableView: UITableView!
     
     var pokemon: Pokemon
 
@@ -17,10 +41,13 @@ class PokemonDetailViewController: UIViewController {
         super.viewDidLoad()
         
         
+        abilitiesTableView.dataSource = self
+        abilitiesTableView.delegate = self
         
-        nameLabel.text = pokemon.name
-        image.load(url: pokemon.sprites.front_default)
-        types.text = pokemon.types.reduce("") { "\($0 ?? "") \($1.type.name)" }
+        
+        pokemonNameLabel.text = pokemon.name
+        pokemonImageView.load(url: pokemon.sprites.front_default)
+        pokemonTypingLabel.text = pokemon.types.reduce("") { "\($0 ?? "") \($1.type.name)" }
     }
     
     init?(pokemon: Pokemon, coder: NSCoder) {
@@ -33,5 +60,24 @@ class PokemonDetailViewController: UIViewController {
     }
     
 
+}
+
+extension PokemonDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pokemon.abilities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let ability = pokemon.abilities[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = ability.ability.name
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
 }
 

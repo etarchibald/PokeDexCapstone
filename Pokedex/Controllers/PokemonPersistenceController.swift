0e9cd73387556e4d22b581a4f38456fc4d8355ab
Austin Dobberfuhl <dobberfuhlaustin@gmail.com>
2024-04-22 16:25:21 -0600

    Added save and load functions

--- Pokedex/Controllers/PokemonPersistenceController.swift
+++ Pokedex/Controllers/PokemonPersistenceController.swift
@@ -13,4 +13,42 @@ import Foundation
 
 class PokemonPersistenceController {
     
+    static let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("emojis").appendingPathExtension("json")
+    
+    //fix to use favortie pokemon
+    static func saveToFile(favoritePokemons: [Pokemon]) {
+        let jsonEncoder = JSONEncoder()
+        let encodedPokemons = try? jsonEncoder.encode(favoritePokemons)
+        try? encodedPokemons?.write(to: ArchiveURL, options: .noFileProtection)
+    }
+    
+    //fix to use favortie pokemon
+    static func loadFromFile() -> [Pokemon] {
+        let jsonDecoder = JSONDecoder()
+        if let retrievedPokemonData = try? Data(contentsOf: ArchiveURL),
+           let decodedPokemons = try? jsonDecoder.decode(Array<Pokemon>.self, from: retrievedPokemonData) {
+            return decodedPokemons
+        } else {
+            return []
+        }
+    }
+    
+    static func saveToFile(decks: [Deck]) {
+        let jsonEncoder = JSONEncoder()
+        let encodedDecks = try? jsonEncoder.encode(decks)
+        try? encodedDecks?.write(to: ArchiveURL, options: .noFileProtection)
+    }
+    
+    static func loadFromFile() -> [Deck] {
+        let jsonDecoder = JSONDecoder()
+        if let retrievedDecksData = try? Data(contentsOf: ArchiveURL),
+           let decodedDecks = try? jsonDecoder.decode(Array<Deck>.self, from: retrievedDecksData) {
+            return decodedDecks
+        } else {
+            return []
+        }
+    }
+
+
 }

d3aa6003a818c8258fcc56fb2b49b3eff1933a4b
Austin Dobberfuhl <dobberfuhlaustin@gmail.com>
2024-04-22 10:18:26 -0600

    new file thats it

---
+++ Pokedex/Controllers/PokemonPersistenceController.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonPersistenceController.swift
+//  Pokedex
+//
+//  Created by Austin Dobberfuhl on 4/22/24.
+//
+//file system save changes
+
+//save deck
+//save all favorite pokemon
+
+import Foundation
+
+class PokemonPersistenceController {
+    
+}

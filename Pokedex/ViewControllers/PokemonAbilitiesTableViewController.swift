4eb92cb07b7076e1e186ba69d1e0ba0ddf8c0287
Austin Dobberfuhl <dobberfuhlaustin@gmail.com>
2024-04-29 10:20:24 -0600

    more tableView and Cell work
    look alright for cells, spacing is ok, adding more text doesnt make …

--- Pokedex/ViewControllers/PokemonAbilitiesTableViewController.swift
+++ Pokedex/ViewControllers/PokemonAbilitiesTableViewController.swift
@@ -23,23 +23,23 @@ class PokemonAbilitiesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonAbility", for: indexPath) as! PokemonAbilityTableViewCell
 
-        // Configure the cell...
+        cell.setup()
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
--- Pokedex/ViewControllers/PokemonAbilityTableViewCell.swift
+++ Pokedex/ViewControllers/PokemonAbilityTableViewCell.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class PokemonAbilityTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var abilityNameLabel: UILabel!
+    @IBOutlet weak var effectDescriptionLabel: UILabel!
+    @IBOutlet weak var flavorTextLabel: UILabel!
+    
+    
+    func setup() {
+        abilityNameLabel.text = "Mirror Shield"
+        effectDescriptionLabel.text = "The msioafiualesu fhshiofh esyau fbousrbfefb oaisefb uasbf saubfsuaoefb oyasfbrhgsei ygb eailrubfr kagrajkbf akjsrfb sefjhb aseligb eawkjgb erajgb eig biweafh eaksfuh askelfh ilefh ewiaoyfg aweifg eroyufg afiulh wago erng elrb arvihar uierabf iarhf eiuoarfb aeifgn"
+        flavorTextLabel.text = "alfhilusilg yhao lgeih ou;h;aeyf;owae f;aewfo ;h;gufharguhaerigha hofasfojf hwef jawuipgfh awpief hwaofghsaef osefjp wpeoufghaiwe gosaiejfkasepnfi asha ejfaewfawiefu how jskfuh isapfh aoefij iaupgwaepigfh eg hipweh fwaefo hweipauf haef weifpu hawipf hwepo h"
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code

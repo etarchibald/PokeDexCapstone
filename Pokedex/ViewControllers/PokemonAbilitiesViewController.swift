42aab8c8eeb4072885e78b25c3c849ca055a5c09
Austin Dobberfuhl <dobberfuhlaustin@gmail.com>
2024-04-29 09:25:07 -0600

    working on ability tableView
    added files (tbaleView and cell) and a storyboard

---
+++ Pokedex/ViewControllers/PokemonAbilitiesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonAbilitiesViewController.swift
+//  Pokedex
+//
+//  Created by Austin Dobberfuhl on 4/29/24.
+//
+
+import Foundation
---
+++ Pokedex/ViewControllers/PokemonAbilityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonAbilityTableViewCell.swift
+//  Pokedex
+//
+//  Created by Austin Dobberfuhl on 4/29/24.
+//
+
+import UIKit
+
+class PokemonAbilityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
